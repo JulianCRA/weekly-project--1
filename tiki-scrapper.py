3c87436af4fa14013c40c399eae7c6aabffd3b8e
Julian R <contact@julianr.dev>
2020-11-21 19:27:29 +0700

    Added average rating.

--- tiki-scrapper.py
+++ tiki-scrapper.py
@@ -5,6 +5,7 @@ import requests
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
 # link = "https://tiki.vn/dien-gia-dung/c1882?src=c.1882.hamburger_menu_fly_out_banner&page=5"
 link = "https://tiki.vn/o-to-xe-may-xe-dap/c8594?src=c.8594.hamburger_menu_fly_out_banner&page=2"
+# link = "https://tiki.vn/search?q=c310%20sony"
 response = requests.get(link, headers=headers)
 html = response.text
 
@@ -20,24 +21,24 @@ for index, product in enumerate(soup.find_all('div', {"class":"product-item"})):
     print("="*50)
     
     #Seller ID
-    print("Seller ID:", product["data-id"])
+    # print("Seller ID:", product["data-id"])
 
-    #Product ID
-    print("Product ID:", product["product-sku"])
+    # #Product ID
+    # print("Product ID:", product["product-sku"])
 
-    #Product price
-    print("Product price:", product["data-price"])
+    # #Product price
+    # print("Product price:", product["data-price"])
 
-    #Product title
-    print("Product title:", product["data-title"])
+    # #Product title
+    # print("Product title:", product["data-title"])
 
-    #Product image
-    product_image = product.find('img', {"class": "product-image"})["src"]
-    print("Product image:", product_image)
+    # #Product image
+    # product_image = product.find('img', {"class": "product-image"})["src"]
+    # print("Product image:", product_image)
 
-    #Product link
-    product_link = "https://tiki.vn/"+re.findall(r'^.*\.html', product.find('a')["href"])[0]
-    print("Product link:", product_link)
+    # #Product link
+    # product_link = "https://tiki.vn/"+re.findall(r'^.*\.html', product.find('a')["href"])[0]
+    # print("Product link:", product_link)
 
     #Tikinow available
     service_badge = product.find('div', {'class': 'badge-service'})
@@ -52,6 +53,10 @@ for index, product in enumerate(soup.find_all('div', {"class":"product-item"})):
     has_reviews = bool(product.find('div', {'class': 'review-wrap'}))
     reviews = re.findall(r'\d+', product.find('p', {'class': 'review'}).text)[0] if has_reviews else 0
     print("# of reviews:", reviews)
+
+    #Rating average
+    average = product.find('span', {'class': 'rating-content'}).span["style"] if has_reviews else 0
+    print("Average score:", 0.05*int(re.findall(r'\d+', average)[0]))
 
     #Underpricing
     is_under_price = bool(product.find('div', {'class': 'badge-under_price'}))
