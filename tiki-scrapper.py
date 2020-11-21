1213912132bb7bf15ab82062ce8507495d6db574
Julian R <contact@julianr.dev>
2020-11-21 13:23:26 +0700

    Changed static test file. Refined reviews fetching method.

--- tiki-scrapper.py
+++ tiki-scrapper.py
@@ -4,12 +4,16 @@ import requests
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
 
-html = requests.get("https://tiki.vn/o-to-xe-may-xe-dap/c8594?src=c.8594.hamburger_menu_fly_out_banner&page=6", headers=headers).text
+response = requests.get("https://tiki.vn/o-to-xe-may-xe-dap/c8594?src=c.8594.hamburger_menu_fly_out_banner&page=7", headers=headers)
+html = response.text
 
 soup = BeautifulSoup(html, features="html.parser")
-
-for product in soup.find_all('div', {"class":"product-item"}):
-    print("-"*50)
+print(len(soup.find_all('div', {"class":"product-item"}))) # if 0 req denied
+for index, product in enumerate(soup.find_all('div', {"class":"product-item"})):
+    print(" "*50)
+    print("="*50)
+    print(f"Product {index}")
+    print("="*50)
     print("Seller ID:", product["data-id"])
     print("Product ID:", product["product-sku"])
     print("Product price:", product["data-price"])
@@ -22,9 +26,15 @@ for product in soup.find_all('div', {"class":"product-item"}):
     print("TikiNow:", is_tiki_now)
     is_freeship = bool(product.find('div', {'class': 'badge-top'}))
     print("Free shipping:", is_freeship)
-    reviews = re.findall(r'\d+', product.find('p', {'class': 'review'}).text)[0]
+    has_reviews = bool(product.find('div', {'class': 'review-wrap'}))
+    reviews = re.findall(r'\d+', product.find('p', {'class': 'review'}).text)[0] if has_reviews else 0
     print("# of reviews:", reviews)
     is_under_price = bool(product.find('div', {'class': 'badge-under_price'}))
     print("Under price:", is_under_price)
     discount = product.find('span', {'class': 'sale-tag'})
     print("Discount:", discount.text if discount else "None")
+    is_paid_by_installments = bool(product.find('div',{'class':'installment-wrapper'}))
+    print("Paid by installments:", is_paid_by_installments)
+    #Freegifts
+    is_freegift = bool(product.find('div',{'class':'freegift-list'}))
+    print("Freegift:",is_freegift) 
